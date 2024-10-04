29caeaba10445699f2cde2e96063687aafd9d929
Ana Carolina Amorim de Souza <132526900+anacamorims@users.noreply.github.com>
2024-10-04 15:13:03 -0400

    Revert "feat: created routes and adding the provider"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import  { router } from "./router/index.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
