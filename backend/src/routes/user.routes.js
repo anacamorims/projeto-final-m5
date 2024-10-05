ef21f98d21ab98aba86b0d981f7cea2afea619cc
Kauã Kelvyn <ckaua504@gmail.com>
2024-10-05 12:18:34 -0300

    ✨ feat: add security system for user authentication
    This commit implements a security system for user authentication using JWT. Users can now securely log in and access their account information with a token. Added middleware to protect routes and ensure users are authenticated.

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createUser, getUser, updateUser, deleteUser } from '../controllers/user.controller.js';
-
+import { createUser, getUser, updateUser, deleteUser, loginUser } from '../controllers/user.controller.js';
+import { authenticateToken } from '../middleware/authMiddleware.js'; 
 const router = express.Router();
 
-router.post('/api/users', createUser); 
-router.get('/api/users/:id', getUser);
-router.put('/api/users/:id', updateUser); 
-router.delete('/api/users/:id', deleteUser); 
+router.post('/api/users', createUser);
+router.post('/api/login', loginUser); 
+router.get('/api/users/:id', authenticateToken, getUser); 
+router.put('/api/users/:id', authenticateToken, updateUser); 
+router.delete('/api/users/:id', authenticateToken, deleteUser);
 
 export default router;
