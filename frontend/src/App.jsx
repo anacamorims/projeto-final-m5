5b983c9491a57fad3dba63016936b90ecc1ed5e4
l1beatriz <beatrizsantosmmg@gmail.com>
2024-10-05 11:12:27 -0300

    ✨new feature: adding home page to routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-import './App.css'
+import { Outlet } from 'react-router-dom'
 
-function App() {
-  return (
-    <h1>Cosmic Bank</h1>
-  )
+export default function App() {
+  return ( <Outlet /> )
 }
 
-export default App
+
--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../pages/home/Home";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
     }
 ])
