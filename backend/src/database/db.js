59db66918a248bb11abb33c88441d1ecbda60092
Kauã Kelvyn <ckaua504@gmail.com>
2024-10-08 20:14:27 -0300

    test

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,47 +1,8 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-import pkg from 'pg';
-
-const { Client } = pkg;
 
 dotenv.config();
 
-// Função para criar o banco de dados se não existir
-const createDatabaseIfNotExists = async () => {
-  try {
-    const client = new Client({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      port: process.env.DB_PORT,
-      ssl: process.env.DB_SSL === 'true' ? {
-        require: true,
-        rejectUnauthorized: false
-      } : false,
-    });
-
-    await client.connect();
-
-    const dbName = process.env.DB_NAME;
-    const checkDbQuery = `SELECT 1 FROM pg_database WHERE datname = '${dbName}'`;
-    const result = await client.query(checkDbQuery);
-
-    if (result.rowCount === 0) {
-      const createDbQuery = `CREATE DATABASE "${dbName}"`;
-      await client.query(createDbQuery);
-      console.log(`Banco de dados "${dbName}" criado com sucesso.`);
-    } else {
-      console.log(`Banco de dados "${dbName}" já existe.`);
-    }
-
-    await client.end();
-  } catch (err) {
-    console.error('Erro ao tentar criar o banco de dados:', err);
-    throw err; 
-  }
-};
-
-// Inicializa o Sequelize
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
@@ -55,25 +16,18 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   }
 });
 
-// Função para criar o banco de dados e as tabelas
-const createDatabaseAndTable = async () => {
+const connectToDatabase = async () => {
   try {
-    await createDatabaseIfNotExists(); // Tenta criar o banco de dados
-
-    await new Promise(resolve => setTimeout(resolve, 2000)); // Espera um pouco para garantir que o banco de dados está pronto
-
-    await sequelize.authenticate(); // Tenta autenticar a conexão
+    await sequelize.authenticate();
     console.log('Conexão com o banco de dados estabelecida com sucesso.');
 
-    await sequelize.sync({ force: false }); // Sincroniza as tabelas
+    await sequelize.sync({ force: false });
     console.log('Tabelas sincronizadas com sucesso.');
   } catch (err) {
     console.error('Não foi possível conectar ao banco de dados:', err);
   }
 };
 
-// Exporta a instância do Sequelize
 export default sequelize;
 
-// Chama a função para criar o banco de dados e as tabelas
-createDatabaseAndTable();
+connectToDatabase();
