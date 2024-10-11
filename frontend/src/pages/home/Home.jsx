d9570e00275b2cf64070bd1560b424b345df30d4
l1beatriz <beatrizsantosmmg@gmail.com>
2024-10-11 12:30:52 -0300

    ✨ new feature: creating page sign up

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,51 +1,51 @@
 import styles from "./home.module.css";
 import { Link } from "react-router-dom";
-import * as React from 'react';
-import { makeStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
-import Card from '../../components/card-bank/card'; // Caminho correto para o arquivo
+// import * as React from 'react';
+// import { makeStyles } from '@mui/styles';
+// import Card from '../../components/card-bank/card'; // Caminho correto para o arquivo
 
-const useStyles = makeStyles({
-  root: {
-    backgroundColor: 'black',
-    color: 'white',
-    '&:hover': {
-      backgroundColor: 'secondary.main',
-    },
-  },
-});
+// const useStyles = makeStyles({
+//   root: {
+//     backgroundColor: 'black',
+//     color: 'white',
+//     '&:hover': {
+//       backgroundColor: 'secondary.main',
+//     }
+//   },
+// });
 
 export default function Home() {
-
-  const classes = useStyles(); 
-
   return (
     <>
-    <section className={styles.homeContainer}>
-      <div className={styles.homeContent}>
-        <div className={styles.homeTitle}>
-          <h1>meet the credit card of the future</h1>
+      <section className={styles.homeContainer}>
+        <div className={styles.homeContent}>
+
+          <div className={styles.homeTitle}>
+            <h1>meet the credit card of the future</h1>
+          </div>
+          
+          <div className={styles.homeText}>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              totam soluta eveniet, laudantium unde dolore doloremque ipsam
+              accusamus commodi quia veritatis. Alias ratione hic cupiditate
+              inventore quisquam maiores facere sint!
+            </p>
+
+            <Link to={"/sign/in"}>
+              <Button 
+                variant="contained" 
+                color="primary"
+                sx={{ boxShadow: "none", borderRadius: 2}}
+              >
+                Saiba mais
+              </Button>
+            </Link>
+            
+          </div>
         </div>
-        <div className={styles.homeText}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            totam soluta eveniet, laudantium unde dolore doloremque ipsam
-            accusamus commodi quia veritatis. Alias ratione hic cupiditate
-            inventore quisquam maiores facere sint!
-          </p>
-          <Link to={"/sign-in"}>
-            <Button variant="contained" color="primary">Saiba mais</Button>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.img}>
-        <Card />
-      </div>
-    </section>
-    <section>
-      dasd
-    </section>
-    
+      </section>
     </>
   );
 }
