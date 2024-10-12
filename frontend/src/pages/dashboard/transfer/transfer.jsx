0230700e08d775d20a3942bf9b2bec498c9d9acc
Kauã Kelvyn <ckaua504@gmail.com>
2024-10-12 15:21:05 -0300

    🎨 feat: add design for bank transfer page

--- frontend/src/pages/dashboard/transfer/transfer.jsx
+++ frontend/src/pages/dashboard/transfer/transfer.jsx
@@ -1,7 +1,60 @@
+import styles from "./transfer.module.css";
+
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import TagRoundedIcon from "@mui/icons-material/TagRounded";
+import AttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
+
 export default function Transfer() {
-    return (
-        <div >
-            <h1>Transfer</h1>
+  return (
+    <section className={styles.container}>
+      <nav className={styles.navbar}>
+        <div className={styles.avatarUser}>
+          <AccountCircleRoundedIcon />
         </div>
-    )
+        <div className={styles.welcomeText}>
+          <h4>Nome de Usuário</h4>
+        </div>
+      </nav>
+
+      <div className={styles.transferContent}>
+        <div className={styles.titleTransfer}>
+          <h2>Transferir</h2>
+        </div>
+
+        <form action="">
+          <div className={styles.input_field}>
+            <input required name="numberAccont" type="number" />
+            <label>Numero da conta</label>
+            <span className={styles.icon}>
+              <TagRoundedIcon />
+            </span>
+          </div>
+          <div className={styles.input_field}>
+            <input required name="numberAccont" type="number" />
+            <label>Valor enviado</label>
+            <span className={styles.icon}>
+              <AttachMoneyRoundedIcon />
+            </span>
+          </div>
+          <div className={styles.textarea_field}>
+            <textarea required name="description"></textarea>
+            <label>Description</label>
+          </div>
+
+          <div className={styles.amount}>
+            <h4>Valor disponivel</h4>
+            <h2>
+              <span>R$</span> 1000.00
+            </h2>
+          </div>
+
+          <div className={styles.transferButton}>
+            <button type="submit">Transferir</button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,31 +4,11 @@ import styles from './home.module.css';
 
 const Home = () => {
   return (
-    <>
-    <section className={styles.homeContainer}>
-      <div className={styles.homeContent}>
-        <div className={styles.homeTitle}>
-          <h1>meet the credit card of the future</h1>
-        </div>
-        <div className={styles.homeText}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            totam soluta eveniet, laudantium unde dolore doloremque ipsam
-            accusamus commodi quia veritatis. Alias ratione hic cupiditate
-            inventore quisquam maiores facere sint!
-          </p>
-          <Link to={"/sign-in"}>
-            <Button variant="contained" color="primary">Saiba mais</Button>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.img}>
-        <Card />
-      </div>
-    </section>
-    <section>
-      dasd
-    </section>
-    </>
+    <div className={styles.container}>
+      <h1>Welcome to My Bank</h1>
+      <Button variant="contained" color="primary">Get Started</Button>
+    </div>
   );
-}
+};
+
+export default Home;
