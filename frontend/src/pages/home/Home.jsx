72ad9c686371a06863d7bf14106a7a8e26a9ca03
Kauã Kelvyn <ckaua504@gmail.com>
2024-10-12 15:35:46 -0300

    🔧 chore: make general adjustments and minor fixes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,52 +1,14 @@
-import styles from "./home.module.css";
-import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
-// import * as React from 'react';
-// import { makeStyles } from '@mui/styles';
-// import Card from '../../components/card-bank/card'; // Caminho correto para o arquivo
+import React from 'react';
+import { Button } from '@mui/material';
+import styles from './home.module.css';
 
-// const useStyles = makeStyles({
-//   root: {
-//     backgroundColor: 'black',
-//     color: 'white',
-//     '&:hover': {
-//       backgroundColor: 'secondary.main',
-//     }
-//   },
-// });
-
-export default function Home() {
+const Home = () => {
   return (
-    <>
-      <section className={styles.homeContainer}>
-        <div className={styles.homeContent}>
-
-          <div className={styles.homeTitle}>
-            <h1>meet the credit card of the future</h1>
-          </div>
-          
-          <div className={styles.homeText}>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-              totam soluta eveniet, laudantium unde dolore doloremque ipsam
-              accusamus commodi quia veritatis. Alias ratione hic cupiditate
-              inventore quisquam maiores facere sint!
-            </p>
-
-            <Link to={"/sign/in"}>
-              <Button 
-                variant="contained" 
-                color="primary"
-                sx={{ boxShadow: "none", borderRadius: 2}}
-              >
-                Saiba mais
-              </Button>
-            </Link>
-            
-          </div>
-        </div>
-      </section>
-    </>
+    <div className={styles.container}>
+      <h1>Welcome to My Bank</h1>
+      <Button variant="contained" color="primary">Get Started</Button>
+    </div>
   );
 };
 
+export default Home;
