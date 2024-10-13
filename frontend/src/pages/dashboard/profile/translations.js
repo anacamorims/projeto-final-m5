bce9d1911ef6b67177d7ef3f0fbbd702adb49bfb
Letícia Borges <leticinhabb4@gmail.com>
2024-10-13 13:05:33 -0300

    🎨 profile page

---
+++ frontend/src/pages/dashboard/profile/translations.js
@@ -0,0 +1,35 @@
+const translations = {
+    en: {
+      title: "Profile",
+      editProfile: "Edit profile information",
+      notifications: "Notifications:",
+      language: "Language",
+      security: "Security",
+      theme: "Theme",
+      helpSupport: "Help & Support",
+      contactUs: "Contact us",
+      privacyPolicy: "Privacy policy",
+      on: "ON",
+      off: "OFF",
+      enable: "Enable",
+      disable: "Disable"
+    },
+    pt: {
+      title: "Perfil",
+      editProfile: "Editar informações do perfil",
+      notifications: "Notificações:",
+      language: "Idioma",
+      security: "Segurança",
+      theme: "Tema",
+      helpSupport: "Ajuda & Suporte",
+      contactUs: "Entre em contato",
+      privacyPolicy: "Política de privacidade",
+      on: "LIGADO",
+      off: "DESLIGADO",
+      enable: "Habilitar",
+      disable: "Desabilitar"
+    }
+  };
+  
+  export default translations;
+  
