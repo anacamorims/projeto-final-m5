b3991957d1d2903483bd9c84e504dee6acafeaea
anacamorims <ana.camorims.dev@gmail.com>
2024-10-13 20:23:56 -0400

    🔧chore: add config server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,35 @@
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./src/docs/swagger.json" assert { type: "json" };
-import userRoutes from "./src/routes/user.routes.js";
-import sequelize from "./src/database/db.js";
-import transationRoutes from "./src/routes/transaction.routes.js";
-import historyRoutes from "./src/routes/history.routes.js";
-import cardRoutes from "./src/routes/card.routes.js";
-import cors from "cors";
+import express from "express"
+import swaggerUi from "swagger-ui-express"
+import { createRequire } from "module"
+import userRoutes from "./src/routes/user.routes.js"
+import transactionRoutes from "./src/routes/transaction.routes.js"
+import historyRoutes from "./src/routes/history.routes.js"
+import cardRoutes from "./src/routes/card.routes.js"
+import sequelize from "./src/database/db.js"
+import cors from "cors"
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(cors());
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(userRoutes);
-app.use(transationRoutes);
-app.use(historyRoutes);
-app.use(cardRoutes);
+const require = createRequire(import.meta.url)
+const swaggerDocument = require("./src/docs/swagger.json")
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use("/users", userRoutes)
+app.use("/transactions", transactionRoutes)
+app.use("/history", historyRoutes)
+app.use("/cards", cardRoutes)
 
 app.listen(PORT, async () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-  await sequelize.authenticate();
-});
+  console.log(`Servidor rodando na porta ${PORT}`)
+  try {
+    await sequelize.authenticate()
+    console.log("Conexão com o banco de dados estabelecida com sucesso.")
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error)
+  }
+})
